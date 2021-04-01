78dcc00472db6730da345f370cc328dba5af568a
LAPTOP-BAD7TPO8\viktor <viktor.varga807@gmail.com>
2021-04-01 16:58:32 +0200

    adding recepies to food.php and working on css

--- food.php
+++ food.php
@@ -12,7 +12,7 @@
 		<h3>Reggelik</h3>
 		<div class="recepies">
 			<div class="left-box">
-				<img src="img/epres_zabszelet.jpg" class="pic">
+				<img src="img/epres_zabszelet.jpg" class="sizable-pic">
 			</div>
 			<div class="right-box">
 				<h4>Hozzávalók:</h4>
@@ -51,6 +51,65 @@
 		<h4>Avokádós-tojásos pirítós elkészítés:</h4>
 		<p>
 		A kenyérszeleteket pirítsd meg, közben olívaolajon kezd sütni a tojásokat. Egy kis sótól és borstól csak még finomabb lesz. Amíg sülnek a tojások, hámozd meg az avokádót, távolítsd el a magját és villával törd össze a húsát, csepegtess rá néhány csepp citromot, hogy ne barnuljon meg. Ahogy elkészültek a pirítósok, kend meg velük a kenyereket, mehet rá a tojás és díszítésnek egy csipetnyi pirospaprika és snidling. Ennyi az egész. Néhány perc és fejedelmi, egészséges reggelit eszik a család.
+		</p>
+		<br>
+		<div class="recepies">
+			<div class="left-box">
+				<img src="img/medvehagymas_pesto.jpg" class="sizable-pic">
+			</div>
+			<div class="right-box">
+				<h4>Hozzávalók:</h4>
+				<ul>
+					<li>100 g dió</li>
+					<li>100 g medvehagyma</li>
+					<li>50 g parmezán</li>
+					<li>1 gerezd fokhagyma</li>
+					<li>350 ml olivaolaj</li>
+				</ul>
+			</div>
+		</div>
+		<h4>Medvehagymás pesto elkészítése:</h4>
+		<p>A diót sütőben pirítsd pár percig, majd ha kihűlt, a kezed között vagy konyharuhában dörzsöld össze őket, így a barna hártyának egy részét el tudod távolítani. Ezzel a módszerrel a keserű íz kevésbé lesz érezhető a masszában. Tedd aprítógépbe a hozzávalókat, az olaj kivételével és daráld össze őket. Amikor homogén szemcsés anyagot kapsz, kezdjed el kisebb adagonként az olajat is eldolgozni benne. Ha paszta állagú masszát kaptál, akkor kanalazd ki kisebb befőttesüvegekbe.
+		A tetejére vékony rétegben még önthetsz egy kevés olajat (így jobban eláll), ezután tedd el a hűtőbe.
+		</p>
+		<div class="recepies">
+			<div class="left-box">
+				<img src="img/tojasos_melegszendvics.png" class="sizable-pic">
+			</div>
+			<div class="right-box">
+				<h4>Hozzávalók:</h4>
+				<ul>
+					<li>4 tojás</li>
+					<li>4 szelet teljes kiőrlésű kenyér</li>
+					<li>4 szelet sajt</li>
+					<li>vaj</li>
+					<li>só, bors ízlés szerint</li>
+				</ul>
+			</div>
+		</div>
+		<h4>Tojásos melegszendvics elkészítése:</h4>
+		<p>Vajazd meg a kenyerek mindkét oldalát, majd tedd félre. Egy serpenyőben hevíts fel egy kevés vajat és süsd készre a tükörtojásokat. Közben egy száraz serpenyőt melegíts át és dobd bele a kenyereket. A tetejüket fedd be sajtszeletekkel és pirítsd addig, amíg a sajt meg nem olvad. Ha kész csúsztasd rá az elkészült tükörtojásokat és boríts rá egy sajtos kenyérszeletet.
+		</p>
+		<div class="recepies">
+			<div class="left-box">
+				<img src="img/teli_gyumolcssalata.png" class="sizable-pic">
+			</div>
+			<div class="right-box">
+				<h4>Hozzávalók:</h4>
+				<ul>
+					<li>¼ ananász</li>
+					<li>2 db kiwi</li>
+					<li>3 db mandarin</li>
+					<li>½ gránátalma</li>
+					<li>1 ek méz</li>
+					<li>2 ek fenyőmag</li>
+					<li>friss citromlé</li>
+				</ul>
+			</div>
+		</div>
+		<h4>Téli gyümölcssaláta elkészítése:</h4>
+		<p>Hámozd meg a gyümölcsöket és aprítsd őket tetszőleges méretűre. A mézből és a frissen facsart citromléből készítsd el az öntetet a gyümölcsökre. Keverd össze őket és szórd meg fenyőmagokkal.
+		Elkészítési idő: 10 perc.
 		</p>
 	</div>
 </body>
